59c36a6dee788fee6f2891ebbef138fbefd126b6
DarkForgeTech <58372111+DarkForgeTech@users.noreply.github.com>
2019-12-01 07:17:01 -0800

    Update tao.py

--- darktao/tao.py
+++ darktao/tao.py
@@ -16,18 +16,18 @@ class darktao(commands.Cog):
         random_chapter = random.randint(0, len(chapters) - 1)
         print_chapter(chapters, random_chapter)
 
-    def print_chapter(chapters, index):
+    async def print_chapter(chapters, index):
         chapter_obj = chapters[index]
         chapter_num = chapter_obj['chapter']
         chapter_text = chapter_obj['text']
 
         await ctx.maybe_send_embed(chapter_text)
 
-    def get_chapters():
+    async def get_chapters():
         with open(tao_text_location) as f:
         	return json.loads(f.read())
 
-    def __init__(self, bot):
+    async def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(
             self,
